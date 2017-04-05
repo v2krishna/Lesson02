ae6b7e1e922cff0122a3b93a4d38812e455c127a
Kris V <v2krishna@gmail.com>
2017-04-04 22:37:22 -0500

    Added three if then else js files

---
+++ ifElse02.js
@@ -0,0 +1,13 @@
+//-----------------------------------------------------------------------------
+//					practice 02 if else
+//-----------------------------------------------------------------------------
+var food;
+food = 'Escargot'
+if (food === 'Pizza')
+{
+	console.log('Yummy!');
+}
+else
+{
+	console.log('I would rather have Pizza');
+}
---
+++ ifelse03.js
@@ -0,0 +1,19 @@
+
+//-----------------------------------------------------------------------------
+//					practice 03 if else
+//-----------------------------------------------------------------------------
+var food ;
+food = 'Chicken';
+console.log('Value of food =',food);
+if(food === 'Pizza')
+{
+	console.log('Yummy!');
+}
+else if (food === 'Chicken')
+{
+	console.log('Super Yummy!');
+}
+else
+{
+	console.log('Would rather have Pizza..');
+}

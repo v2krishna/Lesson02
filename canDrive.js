7628a433e25526b1aa38a3b6ca12ba52aadda497
Kris V <v2krishna@gmail.com>
2017-04-04 22:58:04 -0500

    modified canDrive js; added new var hasDriverLicense

--- canDrive.js
+++ canDrive.js
@@ -2,12 +2,14 @@
 //-----------------------------------------------------------------------------
 //					practice 04 canDrive js
 //-----------------------------------------------------------------------------
-var age;
+var age , hasDriversLicense;
 age =16;
-if(age>=16)
+hasDriversLicense = true;
+if(age>=16 && hasDriversLicense)
 {
 	console.log('Can Drive');
 }
+
 else
 {
 	console.log('Call someone to pick you up');
